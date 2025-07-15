980c8c4ff48f4018486f3864beee71b33e2377a6
Krish Raj <76595963+Krish209@users.noreply.github.com>
2025-07-16 00:08:03 +0530

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/images/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ];
-  },
-  images: {
-    minimumCacheTTL: 31536000,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
